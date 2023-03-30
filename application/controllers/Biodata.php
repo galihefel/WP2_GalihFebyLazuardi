a85e01ca33f396b6f9ade16414a21bb9940cf2dc
galihefel <galihfebylazuardi.id@gmail.com>
2023-03-30 17:29:28 +0700

    Penjumlahan

---
+++ application/controllers/Biodata.php
@@ -0,0 +1,13 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Biodata extends CI_Controller {
+
+    public function biodataku()
+    {
+        $this->load->view('biodata');
+    }
+    public function penjumlahan($nilai1, $nilai2) {
+        echo $this->Model_aritmatika->penjumlahan($nilai1, $nilai2);
+    }
+}
---
+++ application/views/biodata.php
@@ -0,0 +1,2 @@
+Nama: Galih feby lazuardi
+Nim: 17200968
